89e51ad3cdf0e85f2b2e6d15462af8e5a744dafc
shubhamsks <shubhamkumar7051@gmail.com>
2019-11-24 19:26:13 +0530

    Removed navbar visual bug

--- src/components/Navigation/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/DrawerToggle/DrawerToggle.js
@@ -2,9 +2,6 @@ import React from "react";
 import classes from './DrawerToggle.module.css';
 const drawerToggle = props => (
     <div onClick={props.clicked} className = {classes.DrawerToggle}>
-        <div></div>
-        <div></div>
-        <div></div>
     </div>
 );
 export default drawerToggle;
