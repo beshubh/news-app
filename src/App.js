8a231533b26a7ef0cff2e490dc702bab85991641
shubhamsks <shubhamkumar7051@gmail.com>
2019-11-24 14:20:30 +0530

    publishing to github pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
-import {Switch,Route,BrowserRouter} from 'react-router-dom';
+import {Switch,Route}from 'react-router-dom';
 import NewsList from "./components/NewsList/NewsList";
 function App() {
   return (
-      <BrowserRouter>
+
           <div className="App">
             <Layout/>
             <Switch>
                 {/*<Route path='/in' component={NewsList}/>*/}
-                {/*<Route path='/sports' component={NewsList}/>*/}
-                {/*<Route path='/technology' component={NewsList}/>*/}
-                {/*<Route path='/' component={NewsList}/>*/}
+                <Route path='/sports' component={NewsList}/>
+                <Route path='/technology' component={NewsList}/>
+                <Route path='/' component={NewsList}/>
             </Switch>
         </div>
-      </BrowserRouter>
+
   );
 }
 
